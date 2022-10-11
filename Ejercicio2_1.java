ab6f9da9eda79b4e46fa0f75189f289e1b3bc8cf
DanielJoBar <jodar.barbero.daniel@gmail.com>
2022-10-11 13:12:57 +0200

    Operaciones simples

---
+++ Ejercicio2_1.java
@@ -0,0 +1,14 @@
+package java_capitulo_2;
+
+
+public class Ejercicio2_1 {
+    public static void main(String[] args){
+    int x =144 ,y=999;
+    System.out.println("x vale : "+x);
+    System.out.println("y vale : "+y);
+    System.out.println("x+y vale : "+(x+y));
+    System.out.println("x-y vale : "+(x-y));
+    System.out.println("x*y vale : "+(x*y));
+    System.out.println("x/y vale : "+((float)x/y));
+    }
+}
